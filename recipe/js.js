83418ee3c81d4c383eb871fe418de828649d3c5c
Code Cademy <dangkimberly26@gmail.com>
2022-08-03 21:57:55 -0700

    addedDom

--- recipe/js.js
+++ recipe/js.js
@@ -4,3 +4,23 @@ open.addEventListener("click", function (){
     alert("Button is clicked")
 });
 document.body.appendChild(open);
+
+document.getElementById("headertag").innerHTML = "Make Sushi Bake with Me";
+
+let accordionMagic = document.getElementsByClassName("accordion");
+let i;
+
+for (i=0; i<accordionMagic.length; i++){
+    accordionMagic[i].addEventListener("click", function(){
+        //highlight the button
+        this.classList.toggle("active");
+
+        // show the hidden and active panel
+        let panel = this.nextElementSibling;
+        if (panel.style.display === "block"){
+            panel.style.display = "none";
+        } else {
+            panel.style.display = "block"
+        }
+    });
+}
