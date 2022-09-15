35e90cfd10f6efc12691fa4903889e6d8e455c72
Code Cademy <dangkimberly26@gmail.com>
2022-09-14 17:19:50 -0700

    fixed questionair bug and onclick bug

--- reactquizproject/client/src/App.js
+++ reactquizproject/client/src/App.js
@@ -1,26 +1,50 @@
-import React from "react";
+import Questionaire from './components/Questionaire';
+import React, {useState, useEffect} from "react";
 import './App.css';
 
-function App() {
+
+function App(props) {
 
   const [data, setData] = React.useState(null);
+  const [questions,setQuestions] = useState([]);
+  const[currentQuestion, setCurrentQuestion]= useState(undefined);
+
+  // const [isDone, setIsDone]= React.useState(false);
+  
+  // function userIsDone(){
+  //   // setIsDone(true)  
+  //   //no return = undefined
+  //   console.log("userisdone has run")
+  //   return 123; 
+  // }
+
 
   React.useEffect(() => {
     fetch("/api")
       .then((res) => res.json())
-      .then((data) => setData(data));
+      .then((data) => {
+      setQuestions(data.results)
+      setCurrentQuestion(data.results[0])
+    });
   }, []);
 
+// if (!data){
+//   return "Loading..."
+// } 
 
+const handleAnswer = (answer) =>{
+  //check for the answer
+  //show another question
+  //change score if correct
+};
 
-  
-  return (
-    <div className="App">
-      <header className="App-header">
-       
-        <p>{!data ? "Loading..." : data.results.reduce((prev, current) => prev + " , " + current.question )}</p>
-      </header>
+  return questions.length > 0 ? (
+    <div className="container">
+        <Questionaire data={currentQuestion} handleAnswer = {handleAnswer}/>
     </div>
+      ) : (
+        <h2 className="text-2xl text-white font-bold">Loading...</h2>
+      
   );
 }
 
--- reactquizproject/client/src/components/Questionaire.js
+++ reactquizproject/client/src/components/Questionaire.js
@@ -6,19 +6,20 @@ const Questionaire = ({handleAnswer, data: {question, correct_answer, incorrect_
 
 const shuffledAnswers= [ correct_answer, ...incorrect_answers].sort(() => Math.random() - 0.5);
     return  (
-<div>
-        <div className='bg-white text-pink-800 p-10 rounded shadow-md '>
-             <h2 className='text-2xl ' dangerouslySetInnerHTML={{ __html:question}}/> 
+        <div>
+            <div className='bg-white text-pink-800 p-10 rounded shadow-md '>
+                <h2 className='text-2xl' dangerouslySetInnerHTML={{ __html:question}}/> 
+            </div>
+            <div className='grid grid-cols-2 gap-6 mt-6'>
+                
+                {shuffledAnswers.map((answer) => (
+                
+                <button 
+                className={`${correct_answer === answer ? 'bg-pink-300' : 'bg-white'} p-4 text-pink-800 font-semibold rounded shadow`} onClick={() => handleAnswer(answer)} > {answer} </button>
+                ))}
+            </div>
         </div>
-        <div className='grid grid-cols-2 gap-6 mt-6'>
-            
-            {shuffledAnswers.map((answer => (
-            
-            <button className={`${correct_answer === answer ? 'bg-pink-300' : 'bg-white'} p-4 text-pink-800 font-semibold rounded shadow`} onClick{() => handleAnswer(answer)}>{answer}</button>
-            ))}
-        </div>
-</div>
-);
+    );
 };
 
 
