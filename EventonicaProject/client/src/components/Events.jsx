6e27a108d486d9e3837d686602726675a9280220
Code Cademy <dangkimberly26@gmail.com>
2022-09-20 12:43:36 -0700

    added event

--- EventonicaProject/client/src/components/Events.jsx
+++ EventonicaProject/client/src/components/Events.jsx
@@ -1,7 +1,8 @@
+import { useReducer } from "react";
 import { useState } from "react";
 
-   //mock events
-   const event1 = {
+//mock events
+const event1 = {
     id: "1",
     name: "Birthday",
     date: "2021-09-01",
@@ -25,13 +26,50 @@ const event3 = {
     category: "Education",
 };
 
+const reducer = (state, action) => {
+    console.log(action, 'this is the action');
+    switch (action.type) {
+      case 'editName':
+        console.log('Logged if the editName action is being dispatched');
+        //it updates the name to the input value
+        return { ...state, name: action.payload };
+  
+      case 'editDescription':
+        return { ...state, description: action.payload };
+  
+      case 'editCategory':
+        return { ...state, category: action.payload };
+
+        case 'editDate':
+        return { ...state, date: action.payload };
+
+        case 'editID':
+        return { ...state, id: action.payload };
+      default:
+        return state;
+    }
+  };
 
 const Events = () => {
+    //state for events
+    const [events, setEvents] = useState([event1,event2,event3]);
 
-    const [events, useEvents] = useState([event1,event2,event3]);
+//initialistate of the form will be empty
+      const initialState = {
+        id: '',
+        name: '',
+        date: null,
+        description: '',
+        category: ''
+      };
 
+      const [state, dispatch] = useReducer(reducer, initialState);
 
- 
+      const handleSubmit = (e) =>{
+        e.preventDefault();
+        //updates list with new event
+        setEvents([...events, state]);
+      }
 
     return (
         <section className="event-management">
@@ -44,25 +82,94 @@ const Events = () => {
                     {events.map((event, index) => {
                         return (
                             <li key={index}>
-                                ID: {event.id},
-                                name: {event.name},
-                                date: {event.date},
-                                description: {event.description},
-                                category: {event.category},
+                                ID:{event.id},
+                                Name: {event.name},
+                                Date:{event.date},
+                                Description:{event.description} ,
+                                Category: {event.category},
+
                             </li>
                         )
-                    }
-                    )}
+                    })}
                 </ul>
 
                 <h3>Add Event</h3>
-                <form id="add-event" action="#">
+                <form id="add-event" action="#" onSubmit={handleSubmit}>
                     <fieldset>
-                        <label>Name</label>
+                        <br></br>
+                        <label>Name: </label>
+                        <br></br>
                         <input
                             type="text"
                             id="add-event-name"
+                            placeholder="Puppy Cat"
+                            value={state.name}
+                            onChange={(e) =>
+                              dispatch({
+                                type: 'editName',
+                                payload: e.target.value
+                              })
+                            }
+                        />
+                        <br></br>
+                        <label>ID: </label>
+                        <br></br>
+                        <input
+                            type="text"
+                            id="add-event-id"
+                            placeholder="1"
+                            value={state.id}
+                            onChange={(e) =>
+                              dispatch({
+                                type: 'editID',
+                                payload: e.target.value
+                              })
+                            }
+                        />
+                        <br></br>
+                        <label>Date: </label>
+                        <br></br>
+                        <input
+                            type="text"
+                            id="add-event-date"
+                            placeholder="02/20/2022"
+                            value={state.date}
+                            onChange={(e) =>
+                              dispatch({
+                                type: 'editDate',
+                                payload: e.target.value
+                              })
+                            }
+                        />
+                        <br></br>
+                        <label>Description: </label>
+                        <br></br>
+                        <input
+                            type="text"
+                            id="add-event-description"
                             placeholder="Virtual corgi meetup"
+                            value={state.description}
+                            onChange={(e) =>
+                              dispatch({
+                                type: 'editDescription',
+                                payload: e.target.value
+                              })
+                            }
+                        />
+                        <br></br>
+                        <label>Category: </label>
+                        <br></br>
+                        <input
+                            type="text"
+                            id="add-event-category"
+                            placeholder="Virtual corgi meetup"
+                            value={state.category}
+                            onChange={(e) =>
+                              dispatch({
+                                type: 'editCategory',
+                                payload: e.target.value
+                              })
+                            }
                         />
                     </fieldset>
                     {/* Add more form fields here */}
@@ -72,5 +179,4 @@ const Events = () => {
         </section>
     )
 }
-
 export default Events;
